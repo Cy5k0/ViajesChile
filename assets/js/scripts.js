6b9fe2d963d3a84285092fb17989383575cf1e75
Cy5k0 <francisco.colomer@gmail.com>
2024-07-16 12:33:45 -0400

    5to Commit habilita boton de envio con condiciones, modifica texto tooltips de destacados

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,3 +18,31 @@ $(document).ready(function () {
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// disable enable boton enviar
+
+const form = document.getElementById('contactForm');
+    const submitButton = form.querySelector('button[type="submit"]');
+
+    form.addEventListener('input', () => {
+        const nombre = document.getElementById('nombre').value;
+        const asunto = document.getElementById('asunto').value;
+        const mensaje = document.getElementById('mensaje').value;
+
+        if (nombre && asunto && mensaje) {
+            submitButton.removeAttribute('disabled');
+        } else {
+            submitButton.setAttribute('disabled', 'true');
+        }
+    });
+
+
+// Muestra mensaje enviado
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        alert('Mensaje enviado exitosamente');
+        
+    });
